3fefd3bf9aa6a7cff285a9d082d56474ff1865f5
tomtan <tomtan20060613@outlook.com>
2024-09-07 20:52:13 +0800

    Update PIDSlideTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDSlideTest.java
@@ -14,7 +14,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class PIDSlideTest extends LinearOpMode {
     //private TelemetryPacket packet = new TelemetryPacket();
     private final Telemetry mTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-    public static int slideHeightTicks = 0;
+    public static int leftSlideHeightTicks = 0;
+    public static int rightSlideHeightTicks = 0;
     public static double setP = 0.1;
     private Motor mFrontLeftSlide;
     private Motor mFrontRightSlide;
@@ -26,29 +27,52 @@ public class PIDSlideTest extends LinearOpMode {
         mFrontLeftSlide = new Motor(hardwareMap, "frontLeftLift", 28, 6000);
         mFrontRightSlide = new Motor(hardwareMap, "frontRightLift", 28, 6000);
 
+        mBackLeftSlide = new Motor(hardwareMap, "backLeftLift", 28, 6000);
+        mBackRightSlide = new Motor(hardwareMap, "backRightLift", 28, 6000);
+
         mFrontLeftSlide.setInverted(false);
         mFrontRightSlide.setInverted(false);
+
+        mBackLeftSlide.setInverted(false);
+        mBackRightSlide.setInverted(true);
 
         mFrontLeftSlide.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         mFrontRightSlide.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
 
+        mBackLeftSlide.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        mBackRightSlide.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
         mFrontLeftSlide.resetEncoder();
         mFrontRightSlide.resetEncoder();
+
+        mBackLeftSlide.resetEncoder();
+        mBackRightSlide.resetEncoder();
 
         mFrontLeftSlide.setPositionCoefficient(setP);
         mFrontRightSlide.setPositionCoefficient(setP);
 
+        mBackLeftSlide.setPositionCoefficient(setP);
+        mBackRightSlide.setPositionCoefficient(setP);
+
         mFrontLeftSlide.setPositionTolerance(10);
         mFrontRightSlide.setPositionTolerance(10);
+
+        mBackLeftSlide.setPositionTolerance(10);
+        mBackRightSlide.setPositionTolerance(10);
 
         mFrontLeftSlide.setRunMode(Motor.RunMode.PositionControl);
         mFrontRightSlide.setRunMode(Motor.RunMode.PositionControl);
 
+        mBackLeftSlide.setRunMode(Motor.RunMode.PositionControl);
+        mBackRightSlide.setRunMode(Motor.RunMode.PositionControl);
+
         waitForStart();
         while (opModeIsActive()) {
-            mFrontLeftSlide.setTargetDistance(slideHeightTicks);
-            mFrontRightSlide.setTargetDistance(slideHeightTicks);
+            mFrontLeftSlide.setTargetDistance(leftSlideHeightTicks);
+            mFrontRightSlide.setTargetDistance(leftSlideHeightTicks);
 
+            mBackLeftSlide.setTargetDistance(rightSlideHeightTicks);
+            mBackRightSlide.setTargetDistance(rightSlideHeightTicks);
 
             if(!mFrontLeftSlide.atTargetPosition()) {
                 mFrontLeftSlide.set(0.3);
@@ -64,8 +88,26 @@ public class PIDSlideTest extends LinearOpMode {
                 mFrontRightSlide.set(0);
             }
 
+            if(!mBackLeftSlide.atTargetPosition()) {
+                mBackLeftSlide.set(0.3);
+            }
+            else {
+                mBackLeftSlide.set(0);
+            }
+
+            if(!mBackRightSlide.atTargetPosition()) {
+                mBackRightSlide.set(0.3);
+            }
+            else {
+                mBackRightSlide.set(0);
+            }
+
+
+
             mTelemetry.addData("FrontLeft Slide Ticks", mFrontLeftSlide.getCurrentPosition());
             mTelemetry.addData("FrontRight Slide Ticks", mFrontRightSlide.getCurrentPosition());
+            mTelemetry.addData("BackLeft Slide Ticks", mBackLeftSlide.getCurrentPosition());
+            mTelemetry.addData("BackRight Slide Ticks", mBackRightSlide.getCurrentPosition());
             mTelemetry.update();
         }
 
